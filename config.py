72adb1f094c38fff4865bc292f620142a500e524
thiagojobson <thiagojobson@users.noreply.github.com>
2020-07-22 15:10:15 -0300

    Mais ajustes no html

--- config.py
+++ config.py
@@ -9,13 +9,13 @@ class Config:
     TESTING = False
     SECRET_KEY = os.environ["SECRET_KEY"]
     SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
-    SQLALCHEMY_POOL_RECYCLE=int(os.environ.get("SQLALCHEMY_POOL_RECYCLE", 299))
-    SQLALCHEMY_POOL_TIMEOUT=int(os.environ.get("SQLALCHEMY_POOL_TIMEOUT", 20))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_POOL_RECYCLE = int(os.environ.get("SQLALCHEMY_POOL_RECYCLE", 299))
+    SQLALCHEMY_POOL_TIMEOUT = int(os.environ.get("SQLALCHEMY_POOL_TIMEOUT", 20))
 
 
 class DevelopmentConfig(Config):
